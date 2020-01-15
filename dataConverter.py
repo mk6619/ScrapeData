aa4e9aba82d2968d335030172d39aef51ed96df5
Mayank Khursija <mk6619@gmail.com>
2020-01-15 13:14:13 +0530

    Code Refactor

--- dataConverter.py
+++ dataConverter.py
@@ -1,54 +1,19 @@
-import pandas
-import requests
-from bs4 import BeautifulSoup
-
-#sends HTTP request to a url
-def getDataFromUrl(url):
-    return requests.get(url)
-
-#parse HTML data using beautiful soup library
-def parseDataUsingHtmlParser(page):
-    return BeautifulSoup(page.content, 'html.parser')
-
-#find tag by its class
-def findTableByClass(soup, tagId, tableClass):
-    return soup.find(tagId, { "class" : tableClass })
-
-#find tag by its occurance index
-def findTableByIndex(soup, tagId, tagIndex):
-    return soup.findAll(tagId)[tagIndex]
-
-#converts table data to list
-def convertTableToList(table):
-    data = []
-    rows = table.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
-    return data
-
-#converts list to json
-#options available for orientation
-"""
-‘split’ : dict like {‘index’ -> [index], ‘columns’ -> [columns], ‘data’ -> [values]}
-‘records’ : list like [{column -> value}, … , {column -> value}]
-‘index’ : dict like {index -> {column -> value}}
-‘columns’ : dict like {column -> {index -> value}}
-‘values’ : just the values array
-‘table’ : dict like {‘schema’: {schema}, ‘data’: {data}} describing the data, and the data component is like orient='records'.
-"""
-def convertListToJson(dataList, orientation = 'none'):
-    df = pandas.DataFrame(dataList)
-    if orientation == 'none':
-        return df.to_json()
-    else:
-        return df.to_json(orient = orientation)
+import utils
 
 #single function to do all work using tag class
 def convertTableToJsonByClass(URL, tableClass, orientation, tagId = "table"):
-    return convertListToJson(convertTableToList(findTableByClass(parseDataUsingHtmlParser(getDataFromUrl(URL)),tagId, tableClass)), orientation)
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByClass(parsedData,tagId, tableClass)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToJson(listData, orientation)
+    return jsonData
 
 #single function to do all work using tag occurance index
 def convertTableToJsonByIndex(URL, tagIndex, orientation, tagId = "table"):
-    return convertListToJson(convertTableToList(findTableByIndex(parseDataUsingHtmlParser(getDataFromUrl(URL)),tagId, tagIndex)), orientation)
+    responseData = utils.getDataFromUrl(URL)
+    parsedData = utils.parseDataUsingHtmlParser(responseData)
+    tableData = utils.findTableByIndex(parsedData,tagId, tagIndex)
+    listData = utils.convertTableToList(tableData)
+    jsonData = utils.convertListToJson(listData, orientation)
+    return jsonData
--- example.py
+++ example.py
@@ -4,5 +4,5 @@ import dataConverter as dTj
 URL = 'https://www.w3schools.com/python/module_requests.asp'
 tableClass = 'w3-table-all notranslate'
 
-#print(dTj.convertTableToJsonByClass(URL, tableClass, 'records'))
-print(dTj.convertTableToJsonByIndex(URL, 0, 'records'))
+print(dTj.convertTableToJsonByClass(URL, tableClass, 'records'))
+#print(dTj.convertTableToJsonByIndex(URL, 0, 'records'))
